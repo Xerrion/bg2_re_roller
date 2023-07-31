b4e9c2aff37376b18b140b09bd879c393351e6fa
Lasse Nielsen <lasse@xerrion.dk>
2023-07-31 12:30:53 +0200

    Introduced more functions for readability

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from pygetwindow import Win32Window
 
 # Constants
 DEBUG: bool = False
-ROLLS_FILE: str = "max_roll.txt"
+MAX_ROLL_FILE: str = "max_roll.txt"
 REROLL_TEMPLATE: str = "assets/reroll.png"
 STORE_TEMPLATE: str = "assets/store.png"
 TESSERACT_CONFIG: str = "--psm 6 -c tessedit_char_whitelist=0123456789"
@@ -38,16 +38,16 @@ def get_game_window() -> Win32Window:
         sys.exit()
 
 
-def load_rolls():
+def load_max_roll() -> int:
     """
-    Function to load previous rolls from a file.
+    Function to load previous max roll from a file.
     """
     try:
-        with open(ROLLS_FILE, "r") as f:
-            return [int(f.read())]
+        with open(MAX_ROLL_FILE, "r") as f:
+            return int(f.read())
     except FileNotFoundError:
-        logging.warning("Could not find max_roll.txt. Starting with an empty list.")
-        return []
+        logging.warning("Could not find max_roll.txt. Starting with 0.")
+        return 0
 
 
 def find_template(image, template) -> tuple:
@@ -58,6 +58,21 @@ def find_template(image, template) -> tuple:
     result = cv2.matchTemplate(gray, template, cv2.TM_CCOEFF_NORMED)
     _, _, _, max_loc = cv2.minMaxLoc(result)
     return max_loc
+
+
+def setup_roi_and_buttons(window_x, window_y):
+    re_roll_template = cv2.imread(REROLL_TEMPLATE, 0)
+    store_template = cv2.imread(STORE_TEMPLATE, 0)
+    screenshot: np.ndarray = np.array(pyautogui.screenshot())
+    re_roll_button_loc: tuple = find_template(screenshot, re_roll_template)
+    re_roll_button_x: int = re_roll_button_loc[0] + int(re_roll_template.shape[1] / 2)
+    re_roll_button_y: int = re_roll_button_loc[1] + int(re_roll_template.shape[0] / 2)
+    store_button_loc: tuple = find_template(screenshot, store_template)
+    store_button_x: int = store_button_loc[0] + int(store_template.shape[1] / 2)
+    store_button_y: int = store_button_loc[1] + int(store_template.shape[0] / 2)
+    roi_x: int = window_x + ROI_X_OFFSET
+    roi_y: int = window_y + ROI_Y_OFFSET
+    return re_roll_button_x, re_roll_button_y, roi_x, roi_y, store_button_x, store_button_y
 
 
 def extract_roll(roi) -> int | None:
@@ -84,33 +99,33 @@ def main():
     """
     Main function to automate the game.
     """
+    # Get the game window
     window: Win32Window = get_game_window()
-    rolls: list = load_rolls()
 
+    # Load previous max roll
+    max_roll: int = load_max_roll()
+
+    # Get the game window coordinates
     window_x, window_y = window.left, window.top
 
+    # Activate the game window
     if not window.isActive:
         window.activate()
 
+    # Wait for the window to be active
     time.sleep(0.2)
 
-    re_roll_template = cv2.imread(REROLL_TEMPLATE, 0)
-    store_template = cv2.imread(STORE_TEMPLATE, 0)
-
-    screenshot: np.ndarray = np.array(pyautogui.screenshot())
-
-    re_roll_button_loc: tuple = find_template(screenshot, re_roll_template)
-    re_roll_button_x: int = re_roll_button_loc[0] + int(re_roll_template.shape[1] / 2)
-    re_roll_button_y: int = re_roll_button_loc[1] + int(re_roll_template.shape[0] / 2)
-
-    store_button_loc: tuple = find_template(screenshot, store_template)
-    store_button_x: int = store_button_loc[0] + int(store_template.shape[1] / 2)
-    store_button_y: int = store_button_loc[1] + int(store_template.shape[0] / 2)
-
-    roi_x: int = window_x + ROI_X_OFFSET
-    roi_y: int = window_y + ROI_Y_OFFSET
+    # Setup the ROI and buttons
+    re_roll_button_x, re_roll_button_y, roi_x, roi_y, store_button_x, store_button_y = setup_roi_and_buttons(
+        window_x, window_y
+    )
 
     while True:
+        # Activate the game window again if it is not active
+        if not window.isActive:
+            window.activate()
+            time.sleep(0.2)
+
         # Check if the CTRL + SPACE keys are pressed to exit the loop
         if keyboard.is_pressed("ctrl+space"):
             break
@@ -132,25 +147,27 @@ def main():
         if roll is None:
             continue
 
-        max_roll = max(rolls) if rolls else roll
-
         logging.info(f"Current max roll: {max_roll}")
         logging.info(f"Current roll: {roll}")
 
         # If new roll is better, store it
         if roll > max_roll:
+            # Update max roll
+            max_roll = roll
+
             logging.info(f"Found a new max roll: {roll}")
             logging.info("Clicking the store button...")
+
+            # Click the store button
             pyautogui.click(store_button_x, store_button_y)
-            with open(ROLLS_FILE, "w") as f:
+
+            # Write the new max roll to a file
+            with open(MAX_ROLL_FILE, "w") as f:
                 logging.info(f"Writing {roll} to max_roll.txt...")
                 f.write(str(roll))
-            # sleep for a bit to prevent clicking the re-roll button before the store button is clicked
-            time.sleep(0.5)
 
-        rolls.append(roll)
-        most_common_roll = Counter(rolls).most_common(1)[0][0]
-        logging.info(f"Most common roll: {most_common_roll}")
+            # Sleep for a bit to prevent clicking the re-roll button before the store button is clicked
+            time.sleep(0.5)
 
         # Exit if a good roll is found
         if roll >= 100:
